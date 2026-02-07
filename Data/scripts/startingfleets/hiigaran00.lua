4b55526d61ced97ff1ce0a36495846d757f20186
chimas rts <chimas.rts@gmail.com>
2026-02-07 03:08:34 -0300

    Add files via upload

--- Data/scripts/startingfleets/hiigaran00.lua
+++ Data/scripts/startingfleets/hiigaran00.lua
@@ -1,1 +1,77 @@
-
+PersistantData = {
+  StrikeGroups = {
+  },
+  Squadrons = {
+    {
+      type = "Hgn_MotherShip",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+    },
+    {
+      type = "Hgn_ResourceCollector",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 6,
+    },
+    {
+      type = "Hgn_Carrier",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+    {
+      type = "hgn_ionfrigate_T1",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+    {
+      type = "hgn_ionfrigate_T2",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+    {
+      type = "hgn_ionfrigate_T3",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+    {
+      type = "hgn_ionfrigate_T4",
+      subsystems = {
+      },
+      shiphold = {
+      },
+      name = "",
+      size = 1,
+      number = 1, 
+    },
+  },
+	Research = {
+	},
+}

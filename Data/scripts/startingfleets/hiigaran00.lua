8b9ad924c0350ed7006f0d0a08a3551125120da1
chimas rts <chimas.rts@gmail.com>
2026-02-08 00:27:35 -0300

    Add additional ion frigate types to starting fleets

--- Data/scripts/startingfleets/hiigaran00.lua
+++ Data/scripts/startingfleets/hiigaran00.lua
@@ -5,8 +5,10 @@ PersistantData = {
     {type = "Hgn_ResourceCollector",subsystems = {},shiphold = {},name = "",size = 1,number = 6,},
     {type = "Hgn_Carrier",subsystems = {},shiphold = {},name = "",size = 1,number = 1,},
     {type = "hgn_ionfrigate_T1",subsystems = {},shiphold = {},name = "",size = 1,number = 1,},
-    -- {type = "hgn_ionfrigate_T2",subsystems = {},shiphold = {},name = "",size = 1,number = 1,},
-    -- {type = "hgn_ionfrigate_T3",subsystems = {},shiphold = {},name = "",size = 1,number = 1,},
-    -- {type = "hgn_ionfrigate_T4",subsystems = {},shiphold = {},name = "",size = 1,number = 1,},
+    {type = "hgn_ionfrigate_T2",subsystems = {},shiphold = {},name = "",size = 1,number = 1,},
+    {type = "hgn_ionfrigate_T3",subsystems = {},shiphold = {},name = "",size = 1,number = 1,},
+    {type = "hgn_ionfrigate_T4",subsystems = {},shiphold = {},name = "",size = 1,number = 1,},
+	{type = "hgn_ionfrigate_T5",subsystems = {},shiphold = {},name = "",size = 1,number = 1,},
   },
 	Research = {},}
+
